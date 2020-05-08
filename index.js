101b192dcfe18e1415fb0850b5ba851c1f94c176
AmoreiraT <thiago.m.hq@gmail.com>
2020-05-08 17:06:28 -0300

    up to date 8-5

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
 function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
-    window.alert(userEmail + "" + userPass);
+   // const promise = auth.createUserWithEmailAndPassword(userEmail.value, userPass.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Cadastrado");
+
 }
