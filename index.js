b3b3133d8de97061a1af546c1f2d8d8f38210a9d
AmoreiraT <thiago.m.hq@gmail.com>
2020-05-08 14:58:53 -0300

    Firebase

---
+++ index.js
@@ -0,0 +1,6 @@
+function login(){
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    window.alert(userEmail + "" + userPass);
+}
