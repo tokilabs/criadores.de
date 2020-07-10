84d21bf94204857a7ff24ea839fdc2b49b20dc7f
AmoreiraT <thiago.m.hq@gmail.com>
2020-07-10 18:57:24 -0300

    upGetStore

---
+++ okayserv.js
@@ -0,0 +1,172 @@
+const firestore = firebase.firestore();
+// const storage = firebase.storage();
+
+const docRef = firestore.doc("addserv/\"titulo\"");
+
+console.log(docRef);
+
+var addedserv = document.createElement("div");
+addedserv.classList.add('serv');
+
+
+var itensserv = document.getElementsByClassName("overlayserv");
+
+// const doc = await docRef.get();
+// const myData;
+
+function getserv() {
+docRef.get().then(function(doc){
+    const myData = doc.data();
+    console.log(myData);
+    var data = myData;
+    return data;
+});
+}
+
+console.log(getserv(data));
+
+
+
+
+// var imageSrc = myData.hotDogImage;
+// var title = myData.hotDogStatus;
+
+// var servContents =
+//     `
+//         <div class="servgroup">
+//             <div class="BoxView"></div>
+//             <div class="bgServ">
+//                 <div class="BoxServ"></div>
+//                 <div class="paddingBG">
+//                     <img id="nomeserv" alt="Image" src=${imageSrc} class="Defimg" />
+//                     <span class="ServTitle" >${title}</span>
+//                     <div class="DetailBox">
+//                         <div class="SoftUse">${softused}</div>
+//                         <div class="DetailL">${detailused}</div>
+//                     </div>
+//                 </div>
+//                 <div class="apagarbtn">
+//                     <button class="apagar" type="button"> Apagar</button>
+//                 </div>
+//                 <div class="BtnAdd">
+//                     <button id="confirm" class="okay" type="button">Confirmar</button>
+//                 </div>
+//             </div>
+//             <div class="bgDetail">
+//                 <div class="detalhes"> Detalhes </div>
+//                 <div class="categoria"> Categoria </div>
+//                 <div class="valor">Valor </div>
+//                 <div class="linei"></div>
+//                 <div class="bolota"></div>
+//                 <div class="categnname">${servcatg}</div>
+//                 <div class="Real"> R$ </div>
+//                 <span class="ServPrice">${price}</span>
+//                 <div class="lineii"></div>
+//                 <div class="dialogbg">
+//                     <textarea class="inpdialog" cols="2" rows="10"
+//                     name="AdicioneAquiLog" type="text" style="border: none;">"${boxtext}"</textarea>
+//                 </div>
+//             </div>
+//         </div>
+//         <script type="text/javascript" src="./addtostorage.js"></script>
+//     `
+// addedserv.innerHTML = servContents;
+// itensserv.append(addedserv);
+
+
+
+
+
+// const doc = await docRef.get();
+// if (!doc.exists) {
+//     console.log('No such document!');
+// } else {
+//     console.log('Document data:', doc.data());
+// }
+
+
+// console.log(db);
+
+
+// function getAllServ() {
+//     const servs = await firebase.collection('addserv').get();
+//     const allserv = [];
+//     servs.forEach(doc => {
+//         allserv[doc.id] = doc.data();
+//     });
+//     return allserv;
+// }
+
+// console.log(getAllServ(allserv));
+
+
+
+docRef.get().then(function (doc) {
+    if (doc && doc.exists) {
+        const myData = doc.data();
+
+        var addedserv = document.createElement("div");
+        addedserv.classList.add('serv');
+
+
+        var itensserv = document.getElementsByClassName("overlayserv");
+
+        var imageSrc = myData.hotDogImage;
+        var title = myData.hotDogStatus;
+
+        var servContents =
+            `
+        <div class="servgroup">
+            <div class="BoxView"></div>
+            <div class="bgServ">
+                <div class="BoxServ"></div>
+                <div class="paddingBG">
+                    <img id="nomeserv" alt="Image" src=${imageSrc} class="Defimg" />
+                    <span class="ServTitle" >${title}</span>
+                    <div class="DetailBox">
+                        <div class="SoftUse">${softused}</div>
+                        <div class="DetailL">${detailused}</div>
+                    </div>
+                </div>
+                <div class="apagarbtn">
+                    <button class="apagar" type="button"> Apagar</button>
+                </div>
+                <div class="BtnAdd">
+                    <button id="confirm" class="okay" type="button">Confirmar</button>
+                </div>
+            </div>
+            <div class="bgDetail">
+                <div class="detalhes"> Detalhes </div>
+                <div class="categoria"> Categoria </div>
+                <div class="valor">Valor </div>
+                <div class="linei"></div>
+                <div class="bolota"></div>
+                <div class="categnname">${servcatg}</div>
+                <div class="Real"> R$ </div>
+                <span class="ServPrice">${price}</span>
+                <div class="lineii"></div>
+                <div class="dialogbg">
+                    <textarea class="inpdialog" cols="2" rows="10"
+                    name="AdicioneAquiLog" type="text" style="border: none;">"${boxtext}"</textarea>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="./addtostorage.js"></script>
+    `
+        addedserv.innerHTML = servContents;
+        itensserv.append(addedserv);
+
+        // outputHeader.innerHTML = cartRowContents;
+        // outputHeader.innerText = "Hot dog status: " + myData.hotDogStatus + myData.hotDogImage;
+    }
+}).catch(function (error) {
+    console.log("error:", error);
+});
+
+// var itensservNames = itensserv.getElementsByClassName('ServTitle');
+// for (var i = 0; i < itensservNames.length; i++) {
+//     if (itensservNames[i].innerText == title) {
+//         alert('Confirme Serviço')
+//         return
+//     }
+// }
